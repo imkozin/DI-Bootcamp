5b3e23b7f980fbb0fa7712787ee3039d573ca26a
imkozin <ivan.kozin@hotmail.com>
2023-05-10 23:48:06 +0300

    daily

---
+++ Week2/Day4/daily_chal.py
@@ -0,0 +1,27 @@
+def text_to_matrix(text):
+    matrix = []
+    count = 0
+    for row in range(int(len(text) / 3)):
+        matrix.append([])
+        for col in range(3):
+            if count > len(text): break
+            matrix[row].append(text[count])
+            count += 1
+    return matrix
+
+def decrypt(matrix):
+    decrypted = ''
+    no_alpha_group = 0
+    for col in range(3):
+        for row in matrix:
+            char = row[col]
+            if char.isalpha():
+                if no_alpha_group > 1 and decrypted.strip() > '': decrypted += ' '
+                decrypted += row[col]
+                no_alpha_group = 0
+            else:
+                no_alpha_group += 1
+    return decrypted
+
+text = '7i3Tsih%xi #sM $a #t%^r!'
+print(decrypt(text_to_matrix(text)))
