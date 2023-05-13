8d82808a672c66dd5e06fa5218637cbfde939dc2
imkozin <ivan.kozin@hotmail.com>
2023-05-13 22:14:21 +0300

    Tic Tac Toe

---
+++ Week2/Day4/exercisesXPGold.py
@@ -0,0 +1,63 @@
+def compute_sum(X):
+    # Convert X to a string
+    X_str = str(X)
+
+    # Compute the values X, XX, XXX, XXXX
+    values = [int(X_str * (i + 1)) for i in range(4)]
+
+    # Compute the sum of the values
+    total_sum = sum(values)
+
+    return total_sum
+
+# Example usage
+X = 3
+result = compute_sum(X)
+print(result)
+
+# Function to calculate age based on the given date of birth
+def get_age(year, month, day):
+    # Hard-code current year and month
+    current_year = 2023
+    current_month = 5
+
+    # Calculate the age
+    age = current_year - year
+
+    # Adjust the age based on the current month and day
+    if current_month < month:
+        age -= 1
+    elif current_month == month and day > current_day:
+        age -= 1
+
+    return age
+
+# Function to check if a person can retire based on gender and date of birth
+def can_retire(gender, date_of_birth):
+    # Split the date of birth into year, month, and day
+    year, month, day = map(int, date_of_birth.split('/'))
+
+    # Get the age using the get_age function
+    age = get_age(year, month, day)
+
+    # Check if the person can retire based on gender and age
+    if gender == 'm' and age >= 67:
+        return True
+    elif gender == 'f' and age >= 62:
+        return True
+    else:
+        return False
+
+# Get user input for gender and date of birth
+gender = input("Enter gender (m/f): ")
+date_of_birth = input("Enter date of birth (yyyy/mm/dd): ")
+
+# Check if the person can retire using the can_retire function
+retirement_eligible = can_retire(gender, date_of_birth)
+
+# Display the result
+if retirement_eligible:
+    print("You can retire!")
+else:
+    print("You are not eligible for retirement yet.")
+
--- Week2/Day5/miniproject.py
+++ Week2/Day5/miniproject.py
@@ -21,15 +21,140 @@
 # After contemplating the question above, think about splitting your code into smaller pieces (functions).
 # Remember to follow the single responsibility principle! each function should do one thing and do it well!
 
+# board = [
+#     [' ', ' ', ' '],
+#     [' ', ' ', ' '],
+#     [' ', ' ', ' '],
+# ]
+# print("*************")
+# for row in board:
+#         print("* " + row[0] + " | " + row[1] + " | " + row[2] + " *")
+#     print("*---|---|---*")
+# print("*************")
 
-board = [
-    ['X', ' ', ' '],
-    [' ', 'X', ' '],
-    ['O', ' ', 'X'],
-]
+import random
 
-print("*************")
-for row in board:
-    print("* " + row[0] + " | " + row[1] + " | " + row[2] + " *")
-    print("*---|---|---*")
-print("*************")
+print("Welcome to Tic Tac Toe")
+print("**********************")
+
+possible_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+rows = 3
+cols = 3
+
+
+def print_board():
+    for x in range(rows):
+        print('\n+---+---+---+')
+        print("|", end='')
+        for y in range(cols):
+            print("", board[x][y], end=" |")
+    print('\n+---+---+---+')
+
+def modify_array(num, turn):
+    num -= 1
+    if num == 0:
+        board[0][0] = turn
+    elif num == 1:
+        board[0][1] = turn
+    elif num == 2:
+        board[0][2] = turn
+    elif num == 3:
+        board[1][0] = turn
+    elif num == 4:
+        board[1][1] = turn
+    elif num == 5:
+        board[1][2] = turn
+    elif num == 6:
+        board[2][0] = turn
+    elif num == 7:
+        board[2][1] = turn
+    elif num == 8:
+        board[2][2] = turn
+
+
+        
+def check_winner(board):
+    # X axes
+    if board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][0] == '0' and board[0][1] == '0' and board[0][2] == '0':
+        print("0 has won!")
+        return "0"
+    elif board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[1][0] == '0' and board[1][1] == '0' and board[1][2] == '0':
+        print("0 has won!")
+        return "0"
+    elif board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[2][0] == '0' and board[2][1] == '0' and board[2][2] == '0':
+        print("0 has won!")
+        return "0"
+    # Y axes
+    elif board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][0] == '0' and board[1][0] == '0' and board[2][0] == '0':
+        print("0 has won!")
+        return "0"
+    elif board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][1] == '0' and board[1][1] == '0' and board[2][1] == '0':
+        print("0 has won!")
+        return "0"
+    elif board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][2] == '0' and board[1][2] == '0' and board[2][2] == '0':
+        print("0 has won!")
+        return "0"
+    # Cross axes
+    elif board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][0] == '0' and board[1][1] == '0' and board[2][2] == '0':
+        print("0 has won!")
+        return "0"
+    elif board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
+        print("X has won!")
+        return "X"
+    elif board[0][2] == '0' and board[1][1] == '0' and board[2][0] == '0':
+        print("0 has won!")
+        return "0"
+    else:
+        return "N"
+
+leave_loop = False
+turn_counter = 0
+
+while(leave_loop == False):
+        # It's a player turn
+        if turn_counter % 2 == 1:
+            print_board()
+            num_picked = int(input("\nChoose a number [1=9]: "))
+            if num_picked >= 1 or num_picked <= 9:
+                modify_array(num_picked, 'X')
+                possible_num.remove(num_picked)
+            else:
+                print("Invalid input. Please try again.")
+            turn_counter += 1
+        # it's computer's turn 
+        else:
+            while True:
+                cpu_turn = random.choice(possible_num)
+                print('\nCpu choice ', cpu_turn)
+                if cpu_turn in possible_num:
+                    modify_array(cpu_turn, '0')
+                    possible_num.remove(cpu_turn)
+                    turn_counter += 1
+                    break
+        winner = check_winner(board)
+        if winner != 'N':
+            print_board()
+            print("\n Game over! Thank you for playing!")
+            break
