8ff7d661e9e058c1d0fb57f11548cf992fc5e315
imkozin <ivan.kozin@hotmail.com>
2023-05-16 22:54:46 +0300

    xp

--- Week3/Day3/exercises/exercisesXP.py
+++ Week3/Day3/exercises/exercisesXP.py
@@ -45,17 +45,50 @@ class Currency:
         self.amount = amount
 
     def __str__(self):
-        return f"{self.amount} {self.currency}"
+        return f"{self.amount} {self.currency}s"
     
     def __int__(self, amount):
         while self.amount != int(amount):
             raise TypeError
         return self.amount
     
+    def __repr__(self):
+        return f"Currency {self.amount} {self.currency}s"
+    
+    def __add__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError(f'Cannot add between Currency type {self.currency} and {other.currency}')
+            else:
+                return self.amount + other.amount
+        else:
+            return self.amount + int(other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError('Cannot add between Currency type <dollar> and <shekel>')
+            else:
+                self.amount += other.amount
+                return self
+
+        else:
+            self.amount += other
+            return self
+
 c1 = Currency('dollar', 5)
 c2 = Currency('dollar', 10)
 c3 = Currency('shekel', 1)
 c4 = Currency('shekel', 10)
 
 print(str(c1))
-print(c2.__int__(10))
+print(int(c1))
+print(repr(c1))
+print(c1 + 5)
+print(c1 + c2)
+print(c1)
+c1 += 5
+print(c1)
+c1 += c2
+print(c1)
+print(c1 + c3)
