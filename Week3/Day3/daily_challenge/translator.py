59ea92a7dbe2fe603855b44990f0ff5f95913e9e
imkozin <ivan.kozin@hotmail.com>
2023-05-16 22:27:43 +0300

    translator

--- Week3/Day3/daily_challenge/translator.py
+++ Week3/Day3/daily_challenge/translator.py
@@ -3,7 +3,15 @@
 # {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
 # You have to recreate the result using a translator module.
 
-from googletrans import Translator, constants
-translator = Translator()
-translation = translator.translate("Bonjour")
-print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+# from googletrans import Translator, constants
+# translator = Translator()
+# translation = translator.translate("Bonjour")
+# print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+# from translate import Translator
+# translator= Translator(from_lang="french",to_lang="english")
+# translation = translator.translate("Bonour")
+# translation = translator.translate("Au revoir")
+# translation = translator.translate("Bienvenue")
+# translation = translator.translate("A bientôt")
+# print translation
