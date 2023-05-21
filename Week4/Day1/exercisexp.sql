e799b1f0bd48e0a6f0f3f529672fcb99ade17a04
imkozin <ivan.kozin@hotmail.com>
2023-05-21 14:19:19 +0300

    exercise xp

---
+++ Week4/Day1/exercisexp.sql
@@ -0,0 +1,48 @@
+-- CREATE TABLE customers (
+-- 	customer_id SERIAL PRIMARY KEY,
+-- 	first_name VARCHAR(100) NOT NULL,
+-- 	last_name VARCHAR(100) NOT NULL
+-- )
+
+-- SELECT * FROM customers
+
+-- INSERT INTO customers (first_name, last_name)
+-- VALUES
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson')
+
+-- SELECT * FROM customers
+-- All customers whose last name is ‘Smith’ (What will be your outcome?).
+-- SELECT * FROM customers WHERE last_name = 'Smith'
+-- output is an empty table
+
+-- All customers whose last name is ‘Jones’.
+-- SELECT * FROM customers WHERE last_name = 'Jones'
+
+-- All customers whose firstname is not ‘Scott’.
+-- SELECT * FROM customers WHERE not first_name = 'Scott' 
+
+-- CREATE TABLE items (
+-- 	item_id SERIAL PRIMARY KEY,
+-- 	item VARCHAR(100) NOT NULL,
+-- 	price SMALLINT NOT NULL DEFAULT 0
+-- )
+
+-- SELECT * FROM items
+-- INSERT INTO items (item, price)
+-- VALUES
+-- ('Small Desk', 100),
+-- ('Large Desk', 300),
+-- ('Fan', 80)
+
+-- All the items
+-- SELECT * FROM items
+
+-- All the items with a price above 80 (80 not included).
+-- SELECT * FROM items WHERE price > 80
+
+-- All the items with a price below 300. (300 included)
+SELECT * FROM items WHERE price <= 300
