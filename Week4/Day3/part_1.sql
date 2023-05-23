029839ed73d34c7c486e68f69e149d787ccf8760
imkozin <ivan.kozin@hotmail.com>
2023-05-24 01:40:13 +0300

    part1

---
+++ Week4/Day3/part_1.sql
@@ -0,0 +1,54 @@
+-- CREATE TABLE Customer (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL
+-- )
+
+-- CREATE TABLE Customer_Profile (
+--     id SERIAL PRIMARY KEY,
+--     isLoggedIn BOOLEAN DEFAULT false,
+--     customer_id INT REFERENCES Customer (id)
+-- )
+
+-- INSERT INTO Customer (first_name, last_name)
+-- VALUES 
+-- ('John', 'Doe'),
+-- ('Jerome', 'Lalu'),
+-- ('Lea', 'Rive')
+
+-- INSERT INTO Customer_Profile (isLoggedIn, customer_id)
+-- VALUES 
+-- ((SELECT true FROM customer
+-- WHERE first_name = 'John'),
+--  (SELECT id FROM customer
+-- WHERE first_name = 'John'))
+
+-- INSERT INTO Customer_Profile (isLoggedIn, customer_id)
+-- VALUES 
+-- ((SELECT false FROM customer
+-- WHERE first_name = 'Emma'),
+--  (SELECT id FROM customer
+-- WHERE first_name = 'Emma'))
+
+-- select * from customer_profile
+-- The first_name of the LoggedIn customers
+-- SELECT c.first_name
+-- FROM customer AS c
+-- JOIN customer_profile AS cp
+-- ON cp.customer_id = c.id
+-- WHERE isLoggedIn = true
+
+-- All the customers first_name and isLoggedIn columns - even the customers those who don’t have a profile.
+-- SELECT c.first_name, cp.isLoggedIn
+-- FROM customer AS c
+-- LEFT JOIN customer_profile AS cp
+-- ON cp.customer_id = c.id
+
+-- The number of customers that are not LoggedIn
+-- SELECT c.id as number
+-- FROM customer AS c
+-- JOIN customer_profile AS cp
+-- ON cp.customer_id = c.id
+-- WHERE isLoggedIn = false or isLoggedIn is NULL 
+
+
