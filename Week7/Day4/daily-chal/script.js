2100d98e47e6c1183ff070e918130d0d3c3fbc89
imkozin <ivan.kozin@hotmail.com>
2023-06-21 21:29:20 +0300

    finished daily

---
+++ Week7/Day4/daily-chal/script.js
@@ -0,0 +1,28 @@
+const planets = [
+    {name: "Mercury", moons: 0}, 
+    {name: "Venus", moons: 0}, 
+    {name: "Earth", moons: 1}, 
+    {name: "Mars", moons: 2}, 
+    {name: "Jupiter", moons: 13}, 
+    {name: "Saturn", moons: 10}, 
+    {name: "Uranus", moons: 5}, 
+    {name: "Neptune", moons: 2}
+];
+
+const getSection = document.querySelector('section');
+
+for (let planet of planets) {
+    const planetDiv = document.createElement('div');
+    planetDiv.classList.add('planet');
+    planetDiv.classList.add(planet.name.toLowerCase());
+
+    for (let i=0; i < planet.moons; i++) {
+        const moonDiv = document.createElement('div');
+        moonDiv.classList.add('moon');
+        moonDiv.style.left = i * 10 + 'px';
+        planetDiv.appendChild(moonDiv);
+    }
+
+    const section = document.querySelector('.listPlanets');
+    section.appendChild(planetDiv);
+}
