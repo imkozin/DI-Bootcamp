19f7b69ae5cdf6a8a5e8cdab97c9bd0d9b8b4923
imkozin <ivan.kozin@hotmail.com>
2023-06-21 19:24:56 +0300

    restructured folder xp

--- Week7/Day4/exercise-xp/script.js
+++ Week7/Day4/exercise-xp/script.js
@@ -222,56 +222,5 @@ function totalVacationCost() {
 // console.log(totalVacationCost())
 
 
-// Exercise 5 : Users
-// Using Javascript:
-// Retrieve the div and console.log it
-console.log(document);
 
-const getDiv = document.getElementById('container');
-console.log(getDiv)
 
-// Change the name “Pete” to “Richard”.
-const changeLi = document.body.children[1].children[1];
-changeLi.textContent = 'Richard';
-
-// Delete the second <li> of the second <ul>.
-const deleteLi = document.body.children[2].children[1].remove();
-
-// Change the name of the first <li> of each <ul> to your name. (Hint : use a loop)
-const allListClass = document.querySelectorAll('ul');
-for (let el of allListClass) {
-    const firstLi = el.querySelector('li');
-    firstLi.textContent = 'Ivan';
-}
-
-// Using Javascript:
-// Add a class called student_list to both of the <ul>'s.
-const getUl = document.getElementsByClassName('list');
-for (let elem of getUl) {
-    elem.classList.add('student_list');
-}
-
-// Add the classes university and attendance to the first <ul>.
-const getFirstUl = document.querySelector('ul');
-let addClasses = getFirstUl.classList.add('university', 'attendance');
-
-console.log(getFirstUl.classList); 
-
-// Add a “light blue” background color and some padding to the <div>.
-getDiv.style.backgroundColor = 'lightblue';
-getDiv.style.padding = '2em';
-
-// Do not display the <li> that contains the text node “Dan”. (the last <li> of the first <ul>)
-const getLastLi = getFirstUl.nextElementSibling.lastElementChild;
-getLastLi.hidden = true;
-
-// Add a border to the <li> that contains the text node “Richard”. (the second <li> of the <ul>)
-getFirstUl.lastElementChild.style.border = '2px solid black';
-
-// Change the font size of the whole body.
-document.body.style.fontSize = '2em';
-
-// Bonus: If the background color of the div is “light blue”, alert “Hello x and y” (x and y are the users in the div).
-if(getDiv.style.backgroundColor === 'lightblue'){
-    alert("Hello Ivan and Richard");
-}
