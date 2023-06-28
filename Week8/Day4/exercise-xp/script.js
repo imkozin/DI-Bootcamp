c25043fa32bef4089be3a717cbb8149142d6b299
imkozin <ivan.kozin@hotmail.com>
2023-06-28 19:08:19 +0300

    finished xp

--- Week8/Day4/exercise-xp/script.js
+++ Week8/Day4/exercise-xp/script.js
@@ -118,3 +118,23 @@ const cow = new Mamal('Lily', 'cow', 'brown and white', 'Moooo')
 console.log(cow.sound('Moooo'))
 
 // For example: Moooo I'm a cow, named Lily and I'm brown and white
+
+var employee = {    // Object we want to destructure
+  firstname: 'Jon',
+  lastname: 'Snow',
+  dateofbirth: '1990'
+};
+
+// // Destructuring the object into variables without 
+// // assigning default values 
+// var { firstname, lastname, country } = employee;
+// console.log("Without setting default values")
+// console.log( firstname, lastname, country);
+
+// // Destructuring the object into variables by 
+// // assigning default values 
+// var { firstname = 'default firstname', 
+//     lastname = 'default lastname', 
+//     country = 'default country' } = employee;
+// console.log("\n After setting default values")
+// console.log( firstname, lastname, country);
