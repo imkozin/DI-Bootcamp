49f2f3c1226c13b566ce965888771d2bb31cfe51
imkozin <ivan.kozin@hotmail.com>
2023-06-28 21:16:40 +0300

    finished gold

--- Week8/Day3/xp-gold/script.js
+++ Week8/Day3/xp-gold/script.js
@@ -43,5 +43,73 @@ console.log(greetingString);
 
 // Turn the trapped number 3 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]] into: [3]
 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
-const extractedNumber = extractNumber(trapped);
+const extractedNumber = trapped.flat(Infinity);
 console.log(extractedNumber);
+
+// Exercise 1: Sum Elements
+// Write a JavaScript program to find the sum of all elements in an array.
+function findSum(arr) {
+  const total = arr.reduce((a,b) => a + b, 0);
+  return total
+}
+
+console.log(findSum([1, 2, 3, 4]))
+
+// Exercise 2 : Remove Duplicates
+// Write a JavaScript program to remove duplicate items in an array.
+function removeDuplicate(arr) {
+  return arr.filter((elem, index) => {
+    return arr.indexOf(elem) === index
+  });
+}
+
+console.log(removeDuplicate(["apple", "mango", "apple",
+"orange", "mango", "mango"]))
+// Exercise 3 : Remove Certain Values
+// Instructions
+// Write a JavaScript function to remove: null, 0, "", false, undefined and NaN values from an array.
+// Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
+// Expected result : [15, -22, 47]
+function removeNone(arr) {
+  return arr.filter(item => {
+    return Boolean(item);
+  })
+}
+console.log(removeNone([NaN, 0, 15, false, -22, '',undefined, 47, null]))
+
+// Exercise 4 : Repeat Please !
+// Write a JavaScript function to concatenate a given string n times (default is 1).
+// Create the repeat function yourself:
+function repeatFunc(str, num) {
+  return str.repeat(num)
+}
+console.log(repeatFunc('Ha!',3));
+// "Ha!Ha!Ha!"
+
+
+// Exercise 5 : Turtle & Rabbit
+
+// Using this code :
+
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+// Line up the Turtle and the Rabbit at the start line.
+// Expected Output:
+
+//     When you write:
+
+    console.log(startLine);
+    console.log(turtle.padStart(startLine.length));
+    console.log(rabbit.padStart(startLine.length));
+
+//     It should look like this:
+
+//     '     ||<- Start line'
+//     '       🐢'
+//     '       🐇'
+
+
+// What happens when you run 
+// turtle = turtle.trim().padEnd(9, '=');
+
