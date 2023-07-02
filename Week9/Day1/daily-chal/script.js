6f25c5c478f51d69acf8febc801f4e8e7e0bed26
imkozin <ivan.kozin@hotmail.com>
2023-07-02 15:39:10 +0300

    finished daily

---
+++ Week9/Day1/daily-chal/script.js
@@ -0,0 +1,36 @@
+// Create a form with two fields (name, last name) and a submit button.
+// When you click the Send button, retrieve the data from the inputs, and append it on the DOM as a JSON string.
+// The output should be:
+const myForm = document.querySelector('form');
+
+myForm.addEventListener('submit', getValues);
+
+// function getValues(event){
+//     event.preventDefault();
+//     // Retrieve the values from the inputs
+//     const inp1 = document.getElementById('fname').value;
+//     const inp2 = document.getElementById('lname').value;
+
+//     // Create an object with the retrieved values
+//     const myObj = {
+//         name : inp1,
+//         lastname : inp2
+//     };
+//     // Convert the object to a JSON string
+//     const jsonString = JSON.stringify(myObj);
+//     // Append the JSON string to the DOM
+//     const myDiv = document.querySelector('div');
+//     myDiv.innerHTML = jsonString;
+// }
+
+function getValues(e) {
+    e.preventDefault();
+    const formData = new FormData(myForm);
+    const entries = formData.entries();
+    const data = Object.fromEntries(entries);
+
+    const dataString = JSON.stringify(data);
+    const p = document.createElement('p');
+    p.innerText = dataString;
+    document.body.appendChild(p);
+}
