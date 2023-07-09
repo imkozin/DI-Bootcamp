fb8f4aed1c532724884a90e65598d341673f8909
imkozin <ivan.kozin@hotmail.com>
2023-07-09 14:21:42 +0300

    Finished XP

---
+++ Week10/Day1/npm-node/main.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 10;
+// const sum = a + b;
+
+// console.log(`Hello you are ${sum}`);
+
+
+let x;
+const c = 6;
+x = a + b;
+const sum = x - c;
+console.log(`Hello you are ${sum}`);
