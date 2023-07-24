81e73cf52a17b02ef2ea740a63e6d1e72a16cb93
imkozin <ivan.kozin@hotmail.com>
2023-07-24 10:30:23 +0300

    review in class

--- Week11/Day2/exercise-xp/home/src/LangApp.js
+++ Week11/Day2/exercise-xp/home/src/LangApp.js
@@ -46,3 +46,77 @@ function App() {
   }
   
   export default App;
+
+//   const App = () => {
+//     const [languages, setLanguages] = useState([
+//       { name: "Php", votes: 0 },
+//       { name: "Python", votes: 0 },
+//       { name: "JavaSript", votes: 0 },
+//       { name: "Java", votes: 0 },
+//     ]);
+  
+//     const vote = (lang) => {
+//       lang.votes++;
+//       setLanguages([...languages])
+//     }
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//         {
+//           languages.map((item,indx)=>{
+//             return (
+//               <div key={indx}>
+//                 {item.name} {item.votes} <button onClick={()=>vote(item)}>Add</button>
+//               </div>
+//             )
+//           })
+//         }
+//         </header>
+//       </div>
+//     );
+//   };
+
+//   export default App;
+
+// import React, {Component} from 'react';
+
+// class Vote extends React.Component {
+//     constructor() {
+//         super();
+//         this.state= {
+//             languages: [
+//                 {name: "Php", votes: 0},
+//                 {name: "Python", votes: 0},
+//                 {name: "JavaSript", votes: 0},
+//                 {name: "Java", votes: 0}
+//             ],
+//         }
+//     }
+
+//     vote = (lang) => {
+//         const {languages} = this.state;
+//         lang.vote++; 
+//         this.setState({languages:[...this.state.languages]})
+//     }
+
+//     render (){
+//         const {languages} = this.state; // without this part
+//         return (
+//             <div className="App">
+//               <header className="App-header">
+//               {
+//                 languages.map((item,indx)=>{ // or this.state.languages
+//                   return (
+//                     <div key={indx}>
+//                       {item.name} {item.votes} <button onClick={()=>vote(item)}>Add</button>
+//                     </div>
+//                   )
+//                 })
+//               }
+//               </header>
+//             </div>
+//           );
+//     }
+// }
+
+// export default Vote;
